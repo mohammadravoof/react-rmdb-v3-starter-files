e1fefdd4432f08840243285537dd2e7f644db8ce
Mohammad Ravoof <140537466+mohammadravoof@users.noreply.github.com>
2023-12-26 20:49:55 +0530

    home and movie

---
+++ react-rmdb/src/ components/Thumb/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Styles
+import { Image } from './Thumb.styles';
+
+const Thumb = ({ image, movieId, clickable }) => (
+
+    <div>
+        {clickable ? (
+        <Link to={`/${movieId}`}>
+        <Image src={image} alt='movie-image'/>
+        </Link>
+        ) : (
+        <Image src={image} alt='movie-image'/>
+        ) }
+
+    </div>
+
+)
+
+export default Thumb;
